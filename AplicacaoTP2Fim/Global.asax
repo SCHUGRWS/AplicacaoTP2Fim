f7bd1691ebae0eebcde407457be73931a630c4dd
Richard Schug <richard.schug@outlook.com>
2017-09-26 20:03:02 -0300

    Adicionar arquivos de projeto.

---
+++ AplicacaoTP2Fim/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AplicacaoTP2Fim.MvcApplication" Language="C#" %>
---
+++ AplicacaoTP2Fim/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicacaoTP2Fim.Startup))]
+namespace AplicacaoTP2Fim
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
